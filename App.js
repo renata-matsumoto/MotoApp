aa764a7e7391dcedf3250c8108859af5ef89caca
Bruna Dias <bruna.dias.silva@usp.br>
2021-08-08 17:38:34 -0300

    corrigindo

--- App.js
+++ App.js
@@ -1,45 +1,31 @@
-import React, {useState} from "react";
-import { StatusBar } from "react-native";
-import { StyleSheet, View, Text} from "react-native";
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import {UserContext} from './src/components/UserContext';
-import Principal1 from "./src/components/Principal1";
-import LoginInterno from './src/components/LoginInterno';
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import LoginInterno from "./src/components/LoginInterno";
+import Perfil from "./src/components/Perfil";
+import HistoricoGanhos from "./src/components/HistoricoGanhos";
+import ConfirmarCorrida from "./src/components/ConfirmarCorrida";
+import Capa from "./src/components/Capa";
+import Apresentacao from "./src/components/Apresentacao";
+import HistoricoHoje from "./src/components/HistoricoHoje";
 
-
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-
-  const [logado, setLogado] = useState(false);
-  const [usuario, setUsuario] = useState(null);
-
-  const logar = async (user) => {
-    setLogado(true);
-    setUsuario(user);
-  }
-
-  const deslogar = async () => {
-    setLogado(false);
-    setUsuario(null);
-  }
-
   return (
     <NavigationContainer>
       <StatusBar style="auto" />
-      {/* <UserContext.Provider value={{ usuario, logar, deslogar }}>
-      {logado ? <Principal1 /> : <LoginInterno />}
-      </UserContext.Provider> */}
-      <Principal1/>
+      <Drawer.Navigator initialRouteName="Apresentação">
+        <Drawer.Screen name="Apresentação" component={Apresentacao} />
+        <Drawer.Screen name="Capa" component={Capa} />
+        <Drawer.Screen name="Login" component={LoginInterno} />
+        <Drawer.Screen name="Confirmar" component={ConfirmarCorrida} />
+        <Drawer.Screen name="Perfil" component={Perfil} />
+        <Drawer.Screen name="Histórico Mensal" component={HistoricoGanhos} />
+        <Drawer.Screen name="Úlimas Corridas" component={HistoricoHoje} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
